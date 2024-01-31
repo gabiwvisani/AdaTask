efbf053d931a45b8bfa7e088493bc6c3662faaa2
Gabriela Whitaker Visani <38706373+gabiwvisani@users.noreply.github.com>
2024-01-31 18:56:04 -0300

    Delete AdaTask/src/main/java directory

--- AdaTask/src/main/java/Repository/PersonalRepository.java
+++
@@ -1,9 +0,0 @@
-package Repository;
-
-
-import Domain.PersonalTask;
-import java.util.ArrayList;
-
-public class PersonalRepository extends TaskRepository<PersonalTask>{
-    final public ArrayList<PersonalTask> tasksListPersonal = new ArrayList<>();
-}
